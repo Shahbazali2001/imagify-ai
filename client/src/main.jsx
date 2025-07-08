0c2b94b5f677d9952fb3165933bd3ca772259c5b
Shahbazali2001 <shahbazali5097855@gmail.com>
2025-07-08 23:49:46 +0500

    Context Provider

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,11 +1,15 @@
 
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import App from './App.jsx'
+import App from './App.jsx' 
 import './index.css'
+import AppContextProvider  from './context/AppContextProvider.jsx'
 
 createRoot(document.getElementById('root')).render(
+  
   <BrowserRouter>
-    <App />
+    <AppContextProvider>
+      <App />
+    </AppContextProvider>
   </BrowserRouter>
 )
