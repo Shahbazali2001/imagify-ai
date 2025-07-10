263123cef32c1a774af42d059fcfc0cfc6ae113c
Shahbazali2001 <shahbazali5097855@gmail.com>
2025-07-11 03:01:19 +0500

    Fixes

--- client/src/components/GenerateButton.jsx
+++ client/src/components/GenerateButton.jsx
@@ -1,13 +1,31 @@
 import React from "react";
 import { assets } from "../assets/assets";
+import { useContext } from "react";
+import AppContext from "../context/AppContext";
+import { useNavigate } from "react-router-dom";
 
 const GenerateButton = () => {
+
+   const { user, setOpen } = useContext(AppContext);
+    const navigate = useNavigate();
+
+    const handleClick = () => {
+      if (user) {
+        navigate("/result");
+      } else {
+        scrollTo(0, 0);
+        setOpen(true);
+      }
+    };
+
   return (
     <div className="flex flex-col items-center justify-center my-24 p-6 md:px-28">
       <h1 className="text-3xl sm:text-4xl font-semibold mb-2 text-center">
         See the magic. Try now
       </h1>
-      <button className="flex items-center gap-2 sm:text-lg text-white bg-black w-auto mt-8 px-12 py-2.5 rounded-full cursor-pointer">
+      <button
+        onClick={handleClick} 
+        className="flex items-center gap-2 sm:text-lg text-white bg-black w-auto mt-8 px-12 py-2.5 rounded-full cursor-pointer">
         Generate Images
         <img className="h-6" src={assets.star_group} alt="" />
       </button>
