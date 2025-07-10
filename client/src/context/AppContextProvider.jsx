52c4e4bf415aebb2cb5751484fd99d28184aedf0
Shahbazali2001 <shahbazali5097855@gmail.com>
2025-07-11 01:48:00 +0500

    Context added for login component

--- client/src/context/AppContextProvider.jsx
+++ client/src/context/AppContextProvider.jsx
@@ -1,12 +1,17 @@
-import { useState } from "react";
 import AppContext from "./AppContext";
 
+import { useState } from "react";
+
+
 const AppContextProvider = ({ children }) => {
-  const [user, setUser] = useState(null);
+  const [user, setUser] = useState(false);
+  const [open, setOpen] = useState(false);
 
   const values = {
     user,
     setUser,
+    open,
+    setOpen
   };
 
   return (
