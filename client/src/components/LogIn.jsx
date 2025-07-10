c6a40e8a4d642ebe19d993e05285e255245f1de0
Shahbazali2001 <shahbazali5097855@gmail.com>
2025-07-10 22:52:55 +0500

    Working on login

---
+++ client/src/components/LogIn.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const LogIn = () => {
+  return (
+    <div className='absolute top-0 left-0 right-0 bottom-0 z-10 backdrop-blur-sm bg-black/30 flex justify-center items-center'>
+        <form action="">
+            <h1>Login</h1>
+
+
+
+        </form>
+    </div>
+  )
+}
+
+export default LogIn
