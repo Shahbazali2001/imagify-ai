62a346cc73eb0bafb95458a5f73e0fa889395232
Shahbazali2001 <shahbazali5097855@gmail.com>
2025-07-11 01:45:52 +0500

    LogIn component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,23 +5,30 @@ import BuyCredit from "./pages/BuyCredit"
 
 // Component imports
 import Navbar from "./components/Navbar"
+import LogIn from "./components/LogIn"
 
 
 // React Router imports
 import { Routes, Route } from "react-router-dom"
 import Footer from "./components/Footer"
 
+// Context
+import { useContext } from "react"
+import AppContext from "./context/AppContext"
+
 function App() {
-  
+
+  const { open } = useContext(AppContext);
 
   return (
     <div className="px-4 sm:px-10 md:px-14 lg:px-28 min-h-screen bg-gradient-to-b from-teal-50 to-orange-50">
-      <Navbar /> 
-        <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/result" element={<Result />} />
-            <Route path="/buy-credit" element={<BuyCredit />} />
-        </Routes>
+      <Navbar />
+        {open && <LogIn/>}
+          <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/result" element={<Result />} />
+              <Route path="/buy-credit" element={<BuyCredit />} />
+          </Routes>
       <Footer />
     </div>
   )
