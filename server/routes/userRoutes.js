ed47cbbeb9ca6daa8f8cdb393e5109d34d4dfa75
Shahbazali2001 <shahbazali5097855@gmail.com>
2025-07-15 00:17:51 +0500

    Updates

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,11 +1,21 @@
 import express from "express";
-import { registerUser, loginUser, logoutUser } from "../controllers/userController.js";
+import { userAuth } from "../middlewares/auth.js";
+import { registerUser, loginUser, logoutUser, userCredits } from "../controllers/userController.js";
+import { generateImage } from "../controllers/imageController.js";
 
 
 const userRouter = express.Router();
 
+// auth routes
 userRouter.post("/register", registerUser);
 userRouter.post("/login", loginUser);
 userRouter.post("/logout", logoutUser);
+
+// credit routes
+userRouter.post("/credits", userAuth, userCredits);
+
+
+// image routes
+userRouter.post("/generate-image", userAuth, generateImage);
 
 export default userRouter;
