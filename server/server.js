d08ed1576a4ce3b29258af3d9b5cac18d62b38fc
Shahbazali2001 <shahbazali5097855@gmail.com>
2025-07-15 23:29:32 +0500

    Cors Updation

--- server/server.js
+++ server/server.js
@@ -10,8 +10,13 @@ const app = express();
 
 // middleware
 app.use(express.json());
-app.use(cors());
-app.use(express.urlencoded({extended: false}));
+app.use(cors(
+    {
+        origin: "http://localhost:5173",
+        credentials: true,
+    }
+));
+app.use(express.urlencoded({extended: true}));
 
 
 
