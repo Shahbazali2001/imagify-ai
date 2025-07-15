38641faf32ab961f9745656f6c80cd922e7abe6d
Shahbazali2001 <shahbazali5097855@gmail.com>
2025-07-15 23:33:33 +0500

    Bug fixes

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,18 +2,38 @@
 import { useContext } from "react";
 import  AppContext  from "../context/AppContext";
 
-
 // Assets
 import { assets } from "../assets/assets";
 
 // React Router
 import { Link, useNavigate } from "react-router-dom";
 
+// Axios and Toast
+import axiosInstance from "../api/axiosInstance";
+import { toast } from "react-toastify";
+
 
 const Navbar = () => {
 
-  const { user, setOpen } = useContext(AppContext);
+  const { user, setUser, setOpen, setToken, credits } = useContext(AppContext);
   const navigate = useNavigate();
+
+
+  const logOutHandler = async () => {
+    try {
+      await axiosInstance.post("/user/logout");
+      localStorage.removeItem("token");
+      setToken(false);
+      setUser(false);
+      navigate("/");
+      toast.success("User logged out successfully");
+    } catch (error) {
+      console.error("Logout error:", error.message);
+      toast.error(error.response.data.message);
+    }
+  };
+
+
 
   return (
     <div className="flex justify-between items-center py-4">
@@ -34,19 +54,19 @@ const Navbar = () => {
             <button onClick={() => navigate("/buy-credit")} className="flex items-center gap-2 bg-blue-100 px-4 sm:px-6 py-1.5 sm:py-3 rounded-full hover:scale-105 transition-all duration-300">
               <img className="w-5 sm:w-6" src={assets.credit_star} alt="" />
               <p className="text-xs sm:text-sm font-medium text-gray-600">
-                Credits Left : 50
+              Credits Left : {credits}
               </p>
             </button>
-            <p className="text-gray-600 max-sm:hidden pl-4">Hi, Alex</p>
+            <p className="text-gray-600 max-sm:hidden pl-4">{user.name}</p>
             <div className="relative group">
               <img
                 className="w-10 drop-shadow"
                 src={assets.profile_icon}
                 alt=""
               />
-              <div className="absolute hidden group-hover:block top-0 right-0 left-0 z-10 text-black rounded pt-12">
-                <ul className="list-none m-0 py-1 px-2 bg-white rounded-md text-sm">
-                  <li className="py-1 px-2 cursor-pointer">Logout</li>
+              <div className="absolute hidden  group-hover:block top-0 right-0 left-0 z-10 text-black rounded pt-12">
+                <ul className=" m-0 py-2 px-4 w-full  border border-gray-200 bg-white rounded-md text-sm">
+                  <li onClick={logOutHandler} className="cursor-pointer">Logout</li>
                 </ul>
               </div>
             </div>
