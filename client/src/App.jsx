358602e0acba2f0304fd23ddde0485bd037ceae1
Shahbazali2001 <shahbazali5097855@gmail.com>
2025-07-15 14:34:18 +0500

    Toastify Component Added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -16,12 +16,17 @@ import Footer from "./components/Footer"
 import { useContext } from "react"
 import AppContext from "./context/AppContext"
 
+// Toast  
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 function App() {
 
   const { open } = useContext(AppContext);
 
   return (
     <div className="px-4 sm:px-10 md:px-14 lg:px-28 min-h-screen bg-gradient-to-b from-teal-50 to-orange-50">
+      <ToastContainer position="bottom-right" autoClose={5000} />
       <Navbar />
         {open && <LogIn/>}
           <Routes>
